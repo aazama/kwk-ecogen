68a20014c6159a90f8b4f137f00c998bd2e3c7d3
ava azama <avaazama@gmail.com>
2018-06-29 15:00:22 +0000

    added about and resources

--- app.rb
+++ app.rb
@@ -16,6 +16,14 @@ class App < Sinatra::Base
     erb :technology
   end
 
+  get '/aboutpage' do
+    erb :aboutpage
+  end
+
+  get '/ecobotresources' do
+    erb :ecobotresources
+  end
+
   post '/results' do
     @answer = params[:doingNow]
     @finalAnswer = giveTechAnswer(@answer)
